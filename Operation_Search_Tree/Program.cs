48c2ae5430f1f2ea4bfdea1c3303592a3537b9bf
Michelle <15143@student.eadania.dk>
2022-03-09 10:02:23 +0100

    setup

---
+++ Operation_Search_Tree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Operation_Search_Tree
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
